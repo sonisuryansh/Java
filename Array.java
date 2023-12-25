9159ddef9ed1b5dc49a59dc0b53bb804a43b0953
Suryansh Soni <146843858+sonisuryansh@users.noreply.github.com>
2023-12-26 01:24:33 +0530

    Update Array.java

--- Array.java
+++ Array.java
@@ -7,26 +7,20 @@ public class Array {
       arr[1]=60;
       arr[2]=90;
       arr[3]=90;
-
         System.out.println("Dicleration +Initialize");
       int []arry={10,20,30,40,50};
-
       //Printing an Array
         System.out.println("Using For loop");
       for(int i=0;i<arr.length;i++){
           System.out.print(arr[i]+" ");
       }
         System.out.println("");
-
-
         System.out.print("Using For-each loop"+"\n");
         for(int a :arry){
             System.out.print(a+" ");
         }
         System.out.println(" ");
-
         // Multidiamensional Array
-
         int [][]a=new int[2][3];
         a[0][0]=60;
         a[0][1]=20;
@@ -34,19 +28,13 @@ public class Array {
         a[1][0]=60;
         a[1][1]=70;
         a[1][2]=50;
-
         System.out.println("Printing Multidiamentional Array Using For loop");
-
         for(int i=0;i<a.length;i++){
             for(int j=0;j<a[i].length;j++){
                 System.out.print(a[i][j]+" ");
             }
             System.out.print("\n");
         }
-
-
-
-
     }
     }
 
